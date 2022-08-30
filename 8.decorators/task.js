37ea4be55ee6e18e227d5d917a2ad276aba2e44e
Edward Ryzhkov <vinny4000@gmail.com>
2022-08-30 18:34:14 +0700

    init commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,39 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+  function wrapper (...args) {
+    let hash = args.join(',');
+    let objectInCache = cache.findIndex((item) => item.hash === hash);
+
+    if (objectInCache != -1) {
+      result = cache[objectInCache].result;
+      console.log("Из кэша: " + result);
+      return "Из кэша: " + result;
+    }
+
+    result = func(...args);
+    cache.push({
+      hash: hash, 
+      result: result});
+    if (cache.length >= 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper
 }
 
-
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+	function wrapper(...args) {
+		wrapper.count++;
+		if (timeoutId == null) {
+			func(...args);
+		}
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {func(...args)}, delay)
+  };
+    wrapper.count = 0;
+    return wrapper  
 }
