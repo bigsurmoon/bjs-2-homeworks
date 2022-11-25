b2a868db494065ff615ffd639b446785c13e05da
Edward Ryzhkov <vinny4000@gmail.com>
2022-11-25 08:12:32 +0600

    completed functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const item = func(arrOfArr[i]);
+    if (item > max) {
+      max = item;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return Math.abs(max - min);
 }
