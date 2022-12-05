e6ac19e47b079553f12fc1407488f8cdb062e660
Edward Ryzhkov <vinny4000@gmail.com>
2022-12-06 05:30:00 +0600

    completed Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  this.marks.push(mark)
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (!this.marks) {
+    this.marks = marks
+  } else {
+    this.marks = this.marks.concat(marks)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  if (!this.marks) { return 'нет оценок' }
+  const sum = this.marks.reduce((acc, mark) => acc + mark);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
